2e4ed899540c89b22585199a0c832bb2ab98e162
ultimate-knight <maaz2607@gmail.com>
2023-07-07 15:51:18 +0530

    more about formatting

---
+++ formatted string.py
@@ -0,0 +1,7 @@
+formatter='{} {} {} {}'
+
+print(formatter.format(1,2,3,4))
+print(formatter.format(True,True,False,False))
+print(formatter.format("hi","bye","bye","bye"))
+print(formatter.format('where','are','you','there'))
+print(formatter.format(formatter,formatter,formatter,formatter))
