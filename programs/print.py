15fb0482b7f2fd270c6513c9d0053e679659aba2
ultimate-knight <maaz2607@gmail.com>
2023-07-07 16:05:59 +0530

    just more printing

---
+++ programs/print.py
@@ -0,0 +1,8 @@
+days="mon tue wed thu fri sat sun"
+months='jan\nfeb\nmar\napr\nmay\njun\njul\naug\nsep\noct\nnov\ndec'
+print('The no. of days in a month are:',days)
+print('The no. of month in a year arer:',months)
+print("""
+HEY WHERE ARE U.
+DO U KNOW WHO I'M.
+DON'T YOU.""")
