3b8d558ed444523cb25a4d8b4fc493daaef18ffd
ultimate-knight <maaz2607@gmail.com>
2023-07-07 16:56:00 +0530

    glimpse of escape sequence

---
+++ programs/backslash.py
@@ -0,0 +1,13 @@
+tabby_cat="\tI'm packed here."
+perry_cat="i'm here\nwhere are u."
+backslash_cat="you\\are\\here\\there"
+fatcat="""
+you know me:
+\twhat is your name
+\ti'm here
+\tthere u are
+"""
+print(tabby_cat)
+print(perry_cat)
+print(backslash_cat)
+print(fatcat)
