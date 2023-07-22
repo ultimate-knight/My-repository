adf6394bcd4bd39553eb3e5f2d9177b580b49ff8
ultimate-knight <maaz2607@gmail.com>
2023-07-23 01:59:09 +0530

    unique.py

---
+++ notes/leetcode.py/unique.py
@@ -0,0 +1,10 @@
+def firstuniqchar(self,s):
+    char_count={}
+    for char in s:
+        char_count[char]=char_count.get(char,0)+1
+    
+    for i in range(len(s)):
+        if char_count[s[i]]==1:
+            return i
+    
+    return -1
