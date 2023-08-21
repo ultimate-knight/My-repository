1938cd6a167e928a5bd7d412f43834791b5c32ef
ultimate-knight <maaz2607@gmail.com>
2023-08-21 21:30:28 +0530

    javascript files

---
+++ gifty/venty.js
@@ -0,0 +1,7 @@
+let admin;         // Declare a variable named 'admin' without assigning a value
+let vain;          // Declare a variable named 'name' without assigning a value
+
+vain = "john";     // Assign the string "john" to the variable 'name'
+admin = vain; // Assign the value of 'name' to the variable 'admin'
+alert(vain)
+alert(admin)
