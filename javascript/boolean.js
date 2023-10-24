c309c2023337d8a8ec4ae00b03e68b9718569564
ultimate-knight <maaz2607@gmail.com>
2023-10-25 00:58:14 +0530

    boolean

--- javascript/boolean.js
+++ javascript/boolean.js
@@ -4,3 +4,15 @@ alert(99==1);
 alert(2!=2);
 let result=4>5;
 alert(result);
+alert('glow'>'wow');
+alert('maaz'>'baqtiyaar');
+alert('1'>2);
+alert('90'<91);
+alert(0===false);
+alert( null > 0 );  // (1) false
+alert( null == 0 ); // (2) false
+alert( null >= 0 ); // (3) true
+alert( undefined > 0 ); // false (1)
+alert( undefined < 0 ); // false (2)
+alert( undefined == 0 ); // false (3)
+
