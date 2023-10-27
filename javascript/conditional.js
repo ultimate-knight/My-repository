90b29edf41381b45dfb6592138faa1434130d130
ultimate-knight <maaz2607@gmail.com>
2023-10-27 11:41:14 +0530

    conditional operator

---
+++ javascript/conditional.js
@@ -0,0 +1,9 @@
+let year = prompt('In which year was ECMAScript-2015 specification published?', '');
+
+if (year == 2015){
+    alert("you are correct");
+} else if (year>2015){
+    alert("you're so smart");
+}else {
+    alert("you're not smart");
+}
---
+++ javascript/operator.js
@@ -0,0 +1,11 @@
+let rubin;
+let age=prompt("How old are you",'');
+
+if (age>18) {
+    rubin=true;
+} else {
+    rubin=false;
+}
+
+alert(rubin);
+
