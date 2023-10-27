bac479330c2c264f5b1fb1fea0acefb82df66ef2
ultimate-knight <maaz2607@gmail.com>
2023-10-27 12:07:55 +0530

    operator is required

---
+++ javascript/operator1.js
@@ -0,0 +1,11 @@
+let age=prompt("what's the age",18);
+
+if (age<3) {
+    message="hi baby";
+} else if (age<18) {
+    alert('hello');
+} else if (age<100) {
+    alert("greetings");
+} else {
+    alert("what an unusual age");
+}
